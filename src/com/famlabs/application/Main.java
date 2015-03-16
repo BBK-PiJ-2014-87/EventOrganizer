169eff12ae1973fea5784da9869941876f98af75
Vladimirs Ivanovs <ivanovs223@gmail.com>
2015-03-16 16:36:17 +0000

    Comments updated

--- src/com/famlabs/application/Main.java
+++ src/com/famlabs/application/Main.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Famlabs Ltd. All rights reserved.
+ * Copyright (c) 2015 Famlabs.com. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  * 
  * This code is free software. You can redistribute it and/or modify it
